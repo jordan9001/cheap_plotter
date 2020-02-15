6e6364e1931c0c33c9d09b3a25ac32a69881c3bb
Jordan Whitehead <jordan9001@gmail.com>
2020-02-15 13:24:00 -0500

    output path bin files now

--- scripts/spiral.py
+++ scripts/spiral.py
@@ -1,6 +1,8 @@
 from PIL import Image
+import struct
 import sys
 import math
+import os.path
 
 def p2px(p, sz):
     return (int(p[0] * sz[0]), int(p[1] * sz[1]))
@@ -91,12 +93,15 @@ def draw_spiral_am(img, rot = 27, step = 0.001):
         x = 0.5 + math.cos(ang) * r
 
         dist += math.sqrt((x - prev[0])**2 + (y - prev[1])**2)
+        #dist += (x - prev[0]) + (y - prev[1])      # for fun, this is mahnatan dist
+        #dist += max((x - prev[0]), (y-prev[1]))    # and this is l-inf
 
         prev = (x,y)
 
         # modulate
-        w = math.sin(dist * 600) * const * math.pi
-        w *= get_val(img, p2px((x,y), img.size))
+        v = get_val(img, p2px((x,y), img.size))
+        w = math.sin(dist * 693) * const * math.pi
+        w *= v
         r += w
 
         # get modulated x,y
@@ -111,25 +116,55 @@ def draw_spiral_am(img, rot = 27, step = 0.001):
         # get next ang
         ang = ang + (step/(const * ang))
     
-    return pts
+    return [pts]
 
 
-def points_to_img(sz, pts, thick=2, lines=True):
-    if len(pts) < 2:
-        return None
+def points_to_img(sz, pts_list, thick=2, lines=True):
 
     ba = bytearray(b"\xff" * (sz[0] * sz[1]))
 
-    if lines:
-        prev = pts[0]
-        for p in pts[1:]:
-            lineto(ba, sz, p2px(prev, sz), p2px(p, sz), thick)
-            prev = p
-    else:
-        for p in pts:
-            dot(ba, sz, p2px(p, sz), thick)
+    for pts in pts_list:
+        if lines and len(pts) >= 2:
+            prev = pts[0]
+            for p in pts[1:]:
+                lineto(ba, sz, p2px(prev, sz), p2px(p, sz), thick)
+                prev = p
+        else:
+            for p in pts:
+                dot(ba, sz, p2px(p, sz), thick)
     
     return Image.frombytes("L", sz, bytes(ba))
+
+MAX_P = 10000
+def points_to_path(pts_list):
+    # file format:  (all little endian)
+    # rest of file len  uint32
+    # number of lines   uint32
+    #   [
+    #       num points  uint32
+    #       [
+    #           x       uint16
+    #           y       uint16
+    #           ...
+    #       ]
+    #       ...
+    #   ] 
+    
+    f = struct.pack("<I", len(pts_list))
+    for pts in pts_list:
+        f += struct.pack("<I", len(pts))
+        for p in pts:
+            f += struct.pack("<HH", int(p[0] * MAX_P), int(p[1] * MAX_P))
+    
+    f = struct.pack("<I", len(f)) + f
+
+    return f
+
+def total_pts(pts_list):
+    n = 0
+    for l in pts_list:
+        n += len(l)
+    return n
 
 def main():
     img_path = "C:/Users/jorda/Downloads/spiralme.png"
@@ -139,11 +174,24 @@ def main():
     img = Image.open(img_path)
     img = img.convert(mode="L") # 8 bit pix, b & w
 
-    pts = draw_spiral_am(img, 42)
-    sbimg = points_to_img(img.size, pts, 1, True)
+    pts_list = draw_spiral_am(img, 27, 0.003)
+    #pts = draw_spiral_am(img, 42, 0.0001)
+    binf = points_to_path(pts_list)
+    print(total_pts(pts_list), "points,", len(binf), "bytes")
+    sbimg = points_to_img(img.size, pts_list, 1, True)
     #Image.blend(img, sbimg, 0.5).show()
     sbimg.show()
-    sbimg.save(img_path + "modified.png")
+    img_name = img_path[:img_path.rfind('.')] + "_spiral"
+    if os.path.exists(img_name + ".png"):
+        i = 0
+        while os.path.exists(img_name + str(i)+".png"):
+            i += 1
+        img_name = img_name + str(i)
+    print(img_name + ".png")
+    sbimg.save(img_name + ".png")
+    print(img_name + ".bin")
+    with open(img_name + ".bin", "wb") as fp:
+        fp.write(binf)
 
 if __name__ == '__main__':
     main()
